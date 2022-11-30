9556fcf0a326606a18a0bca9b28a171394190742
Hyper-terminal <abhisharma001001001@gmail.com>
2022-11-30 23:15:00 +0530

    added summary image

--- src/components/Layout/Navbar/Navbar.js
+++ src/components/Layout/Navbar/Navbar.js
@@ -1,15 +1,16 @@
 import HeaderCart from "./HeaderCart";
-import "./Navbar.css";
+import  "./Navbar.css";
 
 const Navbar = () => {
-	return (
-		<>
-			<header className="header">
-				<h2>ReactMeals</h2>
-				<HeaderCart />
-			</header>
-		</>
-	);
+    return (
+        <>
+            <header className="header">
+                <h2>ReactMeals</h2>
+                <HeaderCart />
+            </header>
+          
+        </>
+    );
 };
 
 export default Navbar;
